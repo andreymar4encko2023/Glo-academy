93196821cbd42b223561c63d036a5c81385bd564
andrey.mar4encko <andrey.mar4encko2023@gmail.com>
2023-01-26 12:02:42 +0300

    выполнил все задания

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log(gfdfdfdf)
+const title = " ",
+    screens = " ",
+    screenPrice = " ",
+    rollback = " ",
+    fullPrice = " ",
+    adaptive = " ";
+
+    alert =( " первое занятия");
+    
+    console.log( 'первое занятие');
